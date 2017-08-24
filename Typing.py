dcf2dcf8e16e3d252b5578db8111c68e5d6cb583
adamkoziol <adam.koziol@inspection.gc.ca>
2017-08-24 08:43:52 -0400

    Cleaned up a bit. Uncommented CLARK. Minor changes

--- Typing.py
+++ Typing.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 import subprocess
-from spadespipeline import depth
-from spadespipeline import fastqCreator
-from spadespipeline import offhours
-from spadespipeline import quality
-from spadespipeline import runMetadata
 from accessoryFunctions.accessoryFunctions import *
 from accessoryFunctions import metadataprinter
 import glob
@@ -29,7 +24,7 @@ def get_genome_info(fastafile):
     return str(num_bases) + 'bp', str(num_contigs), gc_percent
 
 
-def remove_uncecessary_columns(result_file):
+def remove_unnecessary_columns(result_file):
     import csv
     import os
     necessary_columns = ['SampleName', 'N50', 'NumContigs', 'TotalLength', 'ReferenceGenome', 'RefGenomeAlleleMatches',
@@ -51,94 +46,50 @@ def remove_uncecessary_columns(result_file):
                 out_handle.write(','.join(tmp_row) + "\n")
     os.remove(result_file)
 
+
 class RunTyping(object):
-    """
-    def assembly(self):
-        Helper function for file creation (if desired), manipulation, quality assessment,
-        and trimming as well as the assembly
-        import fastqmover
-        # Run the fastq creation script - if argument is provided
-        if self.fastqcreation:
-            self.runmetadata = fastqCreator.CreateFastq(self)
-            # Print the metadata to file
-            metadataprinter.MetadataPrinter(self)
-        # Simple assembly without requiring accessory files (SampleSheet.csv, etc).
-        elif self.basicassembly:
-            from basicAssembly import Basic
-            self.runmetadata = Basic(self)
-        else:
-            # Populate the runmetadata object by parsing the SampleSheet.csv, GenerateFASTQRunStatistics.xml, and
-            # RunInfo.xml files
-            self.runinfo = "{}RunInfo.xml".format(self.path)
-            self.runmetadata = runMetadata.Metadata(self)
-            # Extract the flowcell ID and the instrument name if the RunInfo.xml file was provided
-            self.runmetadata.parseruninfo()
-            # Populate the lack of bclcall and nohup call into the metadata sheet
-            for sample in self.runmetadata.samples:
-                sample.commands = GenObject()
-                sample.commands.nohupcall = 'NA'
-                sample.commands.bclcall = 'NA'
-            # Run the offhours fastq linking script - if argument
-            if self.offhours:
-                offhoursobj = offhours.Offhours(self)
-                offhoursobj.assertpathsandfiles()
-                offhoursobj.numberofsamples()
-            # Move the files
-            else:
-                fastqmover.FastqMover(self)
-        # Print the metadata to file
-        metadataprinter.MetadataPrinter(self)
-    """
 
     def quality(self):
+        """
+        Creates sequence objects and runs the quality assessment (FastQC), and quality trimming (bbduk) on the
+        supplied sequences
+        """
         from spadespipeline import mMLST
         from spadespipeline import quaster
-        from spadespipeline import mash
         from spadespipeline import prodigal
         from spadespipeline import createobject
-        """Creates quality objects and runs the quality assessment (FastQC), and quality trimming (bbduk) on the
-        supplied sequences"""
-        # Run FastQC on the unprocessed fastq files
-        #self.qualityobject.fastqcthreader('Raw')
-        # Perform quality trimming and FastQC on the trimmed files
-        #self.qualityobject.trimquality()
-        # Print the metadata to file
-        #metadataprinter.MetadataPrinter(self)
-        # Run the error correction module
-        #errorcorrection.Correct(self)
-        #metadataprinter.MetadataPrinter(self)
-        # Run FastQC on the unprocessed fastq files
-        #self.qualityobject.fastqcthreader('trimmedcorrected')
-        # Exit if only pre-processing of data is requested
-        #if self.preprocess:
-        #    printtime('Pre-processing complete', starttime)
-        #    quit()
-        # Run spades
-        #spadesRun.Spades(self)
-        # Run CLARK typing on the .fastq and .fasta files
-        from metagenomefilter import automateCLARK
-        # TODO: Test this on a computer with more RAM. CLARK seems to be working, but it isn't going to run on this machine.
-        # automateCLARK.PipelineInit(self)
-        # self.runmetadata = runMetadata.Metadata(self)
-        # self.runmetadata = createobject.ObjectCreation(self)
-        # TODO: Change the copying to subprocess instead of os.system
-        fasta_files = glob.glob(self.path + "*.fasta")
-        for fasta in fasta_files:
-            make_path(fasta.replace(".fasta", ""))
-            print("cp " + fasta + " " + fasta.replace(".fasta", ""))
-            os.system("cp " + fasta + " " + fasta.replace(".fasta", ""))
-        # Now use createObject to make bestassemblyfile.
+        import shutil
+        # Create the objects
         self.runmetadata = createobject.ObjectCreation(self)
-        # runMetadata.Metadata(self)
-        # createobject.ObjectCreation(self)
+        # Determine the amount of physical memory in the system
+        from psutil import virtual_memory
+        mem = virtual_memory()
+        # If the total amount of memory is greater than 100GB (this could probably be lowered), run CLARK
+        if mem.total >= 100000000000:
+            # Run CLARK typing on the .fastq and .fasta files
+            from metagenomefilter import automateCLARK
+            automateCLARK.PipelineInit(self, 'typing')
+        else:
+            printtime('Not enough RAM to run CLARK!', self.start)
+        # Create a list of the files to process
+        fasta_files = glob.glob(self.path + "*.fasta")
+        # For each of the fasta files, create a folder based on the name of the file - 2014-SEQ-0276.fasta will
+        # be placed in a folder named 2014-SEQ-0276
+        for fasta in fasta_files:
+            # Set the name of the folder
+            fasta_dir = os.path.splitext(fasta)[0]
+            # Create the folder
+            make_path(fasta_dir)
+            # Determine the name and extension of the fasta file
+            fastaname = os.path.split(fasta)[-1]
+            # Copy the file into the appropriate folder
+            shutil.copy(fasta, os.path.join(fasta_dir, fastaname))
+        # Perform quality assessments. After each method completes, print the metadata to file
+        # Run gene predictions
         prodigal.Prodigal(self)
         metadataprinter.MetadataPrinter(self)
-        # Run mash
-        # mash.Mash(self, 'mash')
-        # metadataprinter.MetadataPrinter(self)
         # Run rMLST
         mMLST.PipelineInit(self, 'rmlst')
-        # Print the metadata to file
         metadataprinter.MetadataPrinter(self)
         # Run quast assembly metrics
         for sample in self.runmetadata.samples:
@@ -146,11 +97,11 @@ class RunTyping(object):
         quaster.Quast(self)
         # Calculate the depth of coverage as well as other quality metrics using Qualimap
         metadataprinter.MetadataPrinter(self)
-        # depth.QualiMap(self)
-        # Print the metadata to file
-        # metadataprinter.MetadataPrinter(self)
 
     def typing(self):
+        """
+        Process samples with typing methods
+        """
         from spadespipeline import mMLST
         from spadespipeline import GeneSeekr
         from spadespipeline import sixteenS
@@ -161,9 +112,7 @@ class RunTyping(object):
         from spadespipeline import virulence
         from spadespipeline import vtyper
         from coreGenome import core
-        # import coreGenome
         from spadespipeline import sistr
-        # import resfinder
         # Run modules and print metadata to file
         mMLST.PipelineInit(self, 'mlst')
         metadataprinter.MetadataPrinter(self)
@@ -185,11 +134,8 @@ class RunTyping(object):
         sero = GeneSeekr.PipelineInit(self, 'serotype', True, 95, False)
         serotype.Serotype(sero)
         metadataprinter.MetadataPrinter(self)
-        # vir = GeneSeekr.PipelineInit(self, 'virulence', True, 70)
-        # virulence.Virulence(vir)
-        # For some reason this works with E.coli but not Salmonella or Listeria. Apparently that's the way it's supposed
-        # to be.
-        vir = GeneSeekr.PipelineInit(self, 'virulence', True, 80, True)
+        vir = GeneSeekr.PipelineInit(self, 'virulence', True, 70, True)
+        virulence.Virulence(vir)
         # Remove samples that are not Escherichia so virulence finder doesn't attempt to work on them.
         to_be_readded = list()
         to_be_deleted = list()
@@ -215,14 +161,12 @@ class RunTyping(object):
                     sample.general.filenoext = sample.general.filteredfile.split('.')[0]
         vtyper.Vtyper(self, 'vtyper')
         metadataprinter.MetadataPrinter(self)
-        # TODO: Figure out how to make coreGenome stuff work.
         coregen = GeneSeekr.PipelineInit(self, 'coregenome', True, 70, False)
         core.CoreGenome(coregen)
-        # coregenome stuff works up until here, and then fails.
-        # core.AnnotatedCore(self)
+        # Process core genome Escherichia samples
+        core.AnnotatedCore(self)
         # metadataprinter.MetadataPrinter(self)
         sistr.Sistr(self, 'sistr')
-        # res = resfinder.PipelineInit(self, 'resfinder', False, 80)
         res = GeneSeekr.PipelineInit(self, 'resfinder', False, 80, True)
         GeneSeekr.GeneSeekr(res)
         # resfinder.ResFinder(res)
@@ -238,34 +182,20 @@ class RunTyping(object):
         import multiprocessing
         from spadespipeline import versions
         import time
-        printtime('Welcome to the CFIA bacterial typing pipeline {}'.format(pipelinecommit.decode('utf-8')), startingtime)
+        printtime('Welcome to the CFIA bacterial typing pipeline', self.starttime)
         # Define variables from the arguments - there may be a more streamlined way to do this
         self.args = args
         self.path = os.path.join(args.path, '')
         self.sequencepath = self.path
         self.start = time.time()
-        # self.offhours = args.offhours
-        # self.fastqcreation = args.fastqcreation
-        # self.fastqdestination = args.destinationfastq
         self.reffilepath = os.path.join(args.referencefilepath, '')
-        # self.forwardlength = args.readlengthforward
-        # self.reverselength = args.readlengthreverse
-        # self.numreads = 1 if self.reverselength == 0 else args.numreads
         self.kmers = args.kmerrange
-        # self.preprocess = args.preprocess
         self.updatedatabases = args.updatedatabases
-        # Define the start time
         self.starttime = startingtime
         self.customsamplesheet = args.customsamplesheet
         if self.customsamplesheet:
             assert os.path.isfile(self.customsamplesheet), 'Cannot find custom sample sheet as specified {}'\
                 .format(self.customsamplesheet)
-
-        # self.basicassembly = args.basicassembly
-        # if not self.customsamplesheet and not os.path.isfile("{}SampleSheet.csv".format(self.path)):
-        #    self.basicassembly = True
-        #    printtime('Could not find a sample sheet. Performing basic assembly (no run metadata captured)',
-        #              self.starttime)
         # Use the argument for the number of threads to use, or default to the number of cpus in the system
         self.cpus = args.threads if args.threads else multiprocessing.cpu_count()
         # Assertions to ensure that the provided variables are valid
@@ -274,17 +204,14 @@ class RunTyping(object):
         self.reportpath = '{}reports'.format(self.path)
         assert os.path.isdir(self.reffilepath), u'Reference file path is not a valid directory {0!r:s}'\
             .format(self.reffilepath)
-        # self.miseqpath = args.miseqpath
         self.commit = str(pipelinecommit)
         self.homepath = scriptpath
         self.runinfo = ''
+        self.logfile = os.path.join(self.path, 'logfile.txt')
         # Initialise the metadata object
         self.runmetadata = MetadataObject()
         try:
-            # Start the assembly
-            # self.assembly()
             # Create the quality object
-            # self.qualityobject = quality.Quality(self)
             self.quality()
             # Print the metadata to file
             metadataprinter.MetadataPrinter(self)
@@ -301,13 +228,14 @@ class RunTyping(object):
             sample.mapping = GenObject()
             sample.mapping.Bases, sample.mapping.Contigs, sample.mapping.GcPercentage \
                 = get_genome_info(sample.general.bestassemblyfile)
-            #sample.mapping.Contigs = '1'
-            #sample.mapping.Bases = "100000bp1"
+            # sample.mapping.Contigs = '1'
+            # sample.mapping.Bases = "100000bp1"
             sample.mapping.MeanInsertSize = "200"
             sample.mapping.MeanCoveragedata = "30X"
-            #sample.mapping.GcPercentage = "50"
+            # sample.mapping.GcPercentage = "50"
             # sample.coregenome.targetspresent = '3'
             # sample.coregenome.totaltargets = '4'
+            sample.run = GenObject()
             sample.run.Date = "2017-07-27"
             sample.run.InvestigatorName = "Darles Charwin"
             sample.run.TotalClustersinRun = '2'
@@ -322,7 +250,7 @@ class RunTyping(object):
         # Get all the versions of the software used
         versions.Versions(self)
         metadataprinter.MetadataPrinter(self)
-        remove_uncecessary_columns(self.reportpath + "/combinedMetadata.csv")
+        remove_unnecessary_columns(self.reportpath + "/combinedMetadata.csv")
 
 # If the script is called from the command line, then call the argument parser
 if __name__ == '__main__':
@@ -342,36 +270,8 @@ if __name__ == '__main__':
                         action='version', version='%(prog)s commit {}'.format(commit))
     parser.add_argument('path',
                         help='Specify path')
-    #parser.add_argument('-n', '--numreads',
-    #                    default=2,
-    #                    type=int,
-    #                    help='Specify the number of reads. Paired-reads:'
-    #                    ' 2, unpaired-reads: 1. Default is paired-end')
     parser.add_argument('-t', '--threads',
                         help='Number of threads. Default is the number of cores in the system')
-    #parser.add_argument('-o', '--offhours',
-    #                    action='store_true',
-    #                    help='Optionally run the off-hours module that will search for MiSeq runs in progress, wait '
-    #                         'until the run is complete, and assemble the run')
-    #parser.add_argument('-F', '--fastqcreation',
-    #                    action='store_true',
-    #                    help='Optionally run the fastq creation module that will search for MiSeq runs in progress, '
-    #                         'run bcl2fastq to create fastq files, and assemble the run')
-    #parser.add_argument('-d', '--destinationfastq',
-    #                    help='Optional folder path to store .fastq files created using the fastqCreation module. '
-    #                         'Defaults to path/miseqfolder')
-    #parser.add_argument('-m', '--miseqpath',
-    #                    help='Path of the folder containing MiSeq run data folder')
-    #parser.add_argument('-f', '--miseqfolder',
-    #                    help='Name of the folder containing MiSeq run data')
-    #parser.add_argument('-r1', '--readlengthforward',
-    #                    default='full',
-    #                    help='Length of forward reads to use. Can specify "full" to take the full length of forward '
-    #                         'reads specified on the SampleSheet. Defaults to "full"')
-    #parser.add_argument('-r2', '--readlengthreverse',
-    #                    default='full',
-    #                    help='Length of reverse reads to use. Can specify "full" to take the full length of reverse '
-    #                         'reads specified on the SampleSheet. Defaults to "full"')
     parser.add_argument('-r', '--referencefilepath',
                         default='/spades_pipeline/SPAdesPipelineFiles',
                         help='Provide the location of the folder containing the pipeline accessory files (reference '
@@ -383,13 +283,6 @@ if __name__ == '__main__':
                         help='Path of folder containing a custom sample sheet and name of sample sheet file '
                              'e.g. /home/name/folder/BackupSampleSheet.csv. Note that this sheet must still have the '
                              'same format of Illumina SampleSheet.csv files')
-    #parser.add_argument('-b', '--basicassembly',
-    #                    action='store_true',
-    #                    help='Performs a basic de novo assembly, and does not collect run metadata')
-    #parser.add_argument('-p', '--preprocess',
-    #                    action='store_true',
-    #                    help='Perform quality trimming and error correction only. Do not assemble the trimmed + '
-    #                         'corrected reads')
     parser.add_argument('-u', '--updatedatabases',
                         action='store_true',
                         help='Optionally update (r)MLST databases')
@@ -399,8 +292,5 @@ if __name__ == '__main__':
     starttime = time()
     # Run the pipeline
     RunTyping(arguments, commit, starttime, homepath)
-    printtime('Characterisation complete', starttime)
-    quit()
-
-
-
+    # Print a bold, green exit statement
+    print('\033[92m' + '\033[1m' + "\nElapsed Time: %0.2f seconds" % (starttime() - arguments.start) + '\033[0m')
